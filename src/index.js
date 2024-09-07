63c32cf0793a7d0c442f7e407febf6b3edc77483
TheGBO <gabrielthejota@gmail.com>
2024-09-07 16:53:06 -0300

    playing around, made average colour finder.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const fileInput = document.getElementById("fileInput");
 const canvasBuffer = document.getElementById("canvasBuffer");
+const displayColour = document.getElementById("displayColour");
 /**
  * @type {CanvasRenderingContext2D}
  */
 const ctx = canvasBuffer.getContext('2d');
+var sampleSize = 24;
 
 fileInput.addEventListener('change', () =>{
     const fileList = fileInput.files;
@@ -17,10 +19,33 @@ fileInput.addEventListener('change', () =>{
                 const img = new Image();
                 img.src = e.target.result;
                 img.onload = () =>{
-                    canvasBuffer.width = img.width;
-                    canvasBuffer.height = img.height;
+                    canvasBuffer.width = sampleSize;
+                    canvasBuffer.height = sampleSize;
     
-                    ctx.drawImage(img, 0, 0);
+                    ctx.drawImage(img, 0, 0, canvasBuffer.width, canvasBuffer.height);
+                    const imageData = ctx.getImageData(0, 0, canvasBuffer.width, canvasBuffer.height).data;
+                    let pixels = [];
+                    for (let i = 0; i < imageData.length; i += 4) {
+                        const rgba = [
+                            imageData[i],
+                            imageData[i + 1],
+                            imageData[i + 2],
+                            imageData[i + 3]
+                        ];
+                        pixels.push(rgba);
+                    }
+                    
+                    let allPixelSum = [0,0,0,0];
+                    for (let i = 0; i < pixels.length; i++) {
+                        const pixel = pixels[i];
+                        allPixelSum = allPixelSum.map((num, index) => num+pixel[index]);
+                        console.log(pixel);
+                    }
+                    console.log(allPixelSum)
+                    const finalColour = `rgba(${allPixelSum[0]/pixels.length}, ${allPixelSum[1]/pixels.length}, ${allPixelSum[2]/pixels.length}, ${allPixelSum[3]/pixels.length})`;
+                    displayColour.style.backgroundColor = finalColour;
+                    console.log(finalColour);
+                    
                 }
                 
             }
